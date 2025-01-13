cb80f81f6993e0ac6c5478e4b5275d66f9c946fb
K-rolls <krolls@mun.ca>
2025-01-13 13:32:39 -0330

    initial commit

---
+++ L1/part1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int add(int a, int b){
+    return a + b;
+}
+float add(float a, float b){
+    return a + b;
+}
+
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
+
